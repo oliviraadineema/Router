cc32d068d19dcdb8f15ee7a4fd35bd70c5170175
EZHAR SAVERO <olivira_adineema_29rpl@student.smktelkom-mlg.sch.id>
2022-03-23 10:51:35 +0700

    BukuList

---
+++ src/pages/about.js
@@ -0,0 +1,7 @@
+function About(){
+    return(
+        <div className="mb-4 text-xl fon-semibold">Hello dari About</div>
+    )
+}
+
+export default About;
---
+++ src/pages/home.js
@@ -0,0 +1,7 @@
+function Home(){
+    return(
+        <div className="mb-4 text-xl fon-semibold">Hello World</div>
+    )
+}
+
+export default Home;
